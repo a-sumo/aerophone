182433cda7879e82c06056f4da3a4631378a0145
a-sumo <armandsumo@gmail.com>
2023-10-03 00:25:30 +0200

    remove output images

--- automaton_2d/automaton_2d.c
+++ automaton_2d/automaton_2d.c
@@ -3,32 +3,44 @@
 
 void compute_next_state(uint8_t** current_state, uint8_t** next_state, size_t width, size_t height) {
     for(size_t i = 1; i < height-1; i++) {
-        for(size_t j = 1; j < width-1; j+=8) {  // Processing 8 cells at a time
-            // Load 8 cells for each neighboring position
+        for(size_t j = 1; j < width-1; j+=8) {
+            uint8x8_t center = vld1_u8(&current_state[i][j]);
             uint8x8_t left = vld1_u8(&current_state[i][j-1]);
             uint8x8_t right = vld1_u8(&current_state[i][j+1]);
             uint8x8_t up = vld1_u8(&current_state[i-1][j]);
             uint8x8_t down = vld1_u8(&current_state[i+1][j]);
-            uint8x8_t up_left = vld1_u8(&current_state[i-1][j-1]);
-            uint8x8_t up_right = vld1_u8(&current_state[i-1][j+1]);
-            uint8x8_t down_left = vld1_u8(&current_state[i+1][j-1]);
-            uint8x8_t down_right = vld1_u8(&current_state[i+1][j+1]);
-            uint8x8_t center = vld1_u8(&current_state[i][j]);
 
-            // Convert uint8x8_t to uint16x8_t for sum accumulation
+            // Sum up the neighbors
             uint16x8_t sum = vaddl_u8(center, left);
             sum = vaddw_u8(sum, right);
             sum = vaddw_u8(sum, up);
             sum = vaddw_u8(sum, down);
-            sum = vaddw_u8(sum, up_left);
-            sum = vaddw_u8(sum, up_right);
-            sum = vaddw_u8(sum, down_left);
-            sum = vaddw_u8(sum, down_right);
 
-            // Divide the sum by 9 for averaging and store the result
-            uint8x8_t avg = vshrn_n_u16(sum, 3);  // Approximate division by 9 using right shift by 3
+            uint8x8_t avg = vshrn_n_u16(sum, 2);  // Divide by 4 using right shift by 2
             vst1_u8(&next_state[i][j], avg);
         }
     }
 }
 
+
+void apply_boundary_conditions(uint8_t** state, size_t width, size_t height) {
+    float decay_factor = 0.85;
+    for(size_t i = 0; i < height; i++) {
+        state[i][0] *= decay_factor;
+        state[i][width-1] *= decay_factor;
+    }
+    for(size_t j = 0; j < width; j++) {
+        state[0][j] *= decay_factor;
+        state[height-1][j] *= decay_factor;
+    }
+}
+
+void add_sustained_excitation(uint8_t** state, size_t width, size_t height, size_t timestep) {
+    if(timestep % 367 == 0) {
+        for(size_t i = height/2 - 5; i < height/2 + 5; i++) {
+            for(size_t j = width/2 - 5; j < width/2 + 5; j++) {
+                state[i][j] = 255;
+            }
+        }
+    }
+}
